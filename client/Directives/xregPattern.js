237cdb2814e7f60ffc124f99dfd7b7fdef928af4
Richard L. Wisniewski <r.wisniewski1101@gmail.com>
2016-09-16 23:10:01 -0400

    Found a quick optimisation for directive

--- client/Directives/xregPattern.js
+++ client/Directives/xregPattern.js
@@ -1,19 +1,18 @@
 define([
-	'./Module',
-	'xregexp'
+    './Module',
+    'xregexp'
 ], function(module) {
-	'use strict';
-	return module.directive('xregPattern', function patternTest ($parse) {
-		return {
-		    require: '?ngModel',
-		    restrict: 'A',
-		    link: function(scope, elem, attrs, ctrl) {
-				var regex = XRegExp(attrs.xregPattern)
-				ctrl.$validators.xregPattern = function(){
-					var value = regex.test(ctrl.$viewValue) === true;
-					return value;
-				};
-			}
-		};
-	});
+    'use strict';
+    return module.directive('xregPattern', function patternTest($parse) {
+        return {
+            require: '?ngModel',
+            restrict: 'A',
+            link: function(scope, elem, attrs, ctrl) {
+                var regex = XRegExp(attrs.xregPattern)
+                ctrl.$validators.xregPattern = function() {
+                    return regex.test(ctrl.$viewValue) === true;
+                };
+            }
+        };
+    });
 });
