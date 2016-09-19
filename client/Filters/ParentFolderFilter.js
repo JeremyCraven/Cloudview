b9c954922a925f67605ec75444b4219a9659788e
Richard L. Wisniewski <r.wisniewski1101@gmail.com>
2016-09-18 23:13:44 -0400

    Finished ParentFolderFilters

--- client/Filters/ParentFolderFilter.js
+++ client/Filters/ParentFolderFilter.js
@@ -1,10 +1,22 @@
-define([
+define
     './Module'
 ], function(module) {
     return module.filter('parentFolder', function() {
         return function(items, numLvl) {
-            var filtered = [];
-            
+            var filtered = {
+				list:	[],
+				cutoff: true
+			};
+			if(numLvl > items.legth) {
+				filtered.cutoff = false;
+				filtered.list = items;
+				return filtered;
+			}
+			var smeti = items.reverse();
+			for(var i=0; i<numLvl; i++) {
+				filtered.list.push(smeti[i]);
+			}
+			filtered.list = filtered.lists.reverse();
             return filtered;
         };
     });
