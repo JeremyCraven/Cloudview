3c9c88b6179220af7b4047aa5009827d9e0fb88b
Richard L. Wisniewski <r.wisniewski1101@gmail.com>
2016-09-19 13:07:50 -0400

    Cleaned up Folder UX
    Adjusted toolbar layout to be more polished.

--- client/Controllers/Static/FolderController.js
+++ client/Controllers/Static/FolderController.js
@@ -1,68 +1,73 @@
 define([
-	'./Module'
+    './Module'
 ], function(module) {
-	return module.controller('CloudView.Controllers.Folder', [
-		'$scope',
-		'$state',
-		'$mdSidenav',
-		function FolderController($scope, $state, $mdSidenav) {
-			$scope.user = {
-				hasName:	true,
-				username:	'john.doe123',
-				name:		'John Doe',
-				accounts: [
-					{
-						type:		'Google',
-						username:	'john.doe123@googlemail.com',
-						active: true,
-						space: {
-							used:	1024,
-							available:	15*1024*1024*1024
-						}
-					},
-					{
-						type:		'Google',
-						username:	'John.Doe@work.com',
-						active:		true,
-						space: {
-							used: 960*1024*1024,
-							available: 5*1024*1024*1024
-						}
-					},
-					{
-						type:		'Dropbox',
-						username:	'john.doe123',
-						active:		false,
-						space: {
-							used: 14.5*1024*1024*1024,
-							available: 15*1024*1024*1024
-						}
-					}
-				]
-			};
-			$scope.folder = {
-				title: 'New Folder',
-				account: 'Gmail', //TODO: Bacakend guy figure out datatype
-				path:	['Party', 'Suplies'],
-				subfolders: [],
-				files:	[]
+    return module.controller('CloudView.Controllers.Folder', [
+        '$scope',
+        '$state',
+        '$mdSidenav',
+        function FolderController($scope, $state, $mdSidenav) {
+            $scope.user = {
+                hasName: true,
+                username: 'john.doe123',
+                name: 'John Doe',
+                accounts: [{
+                    type: 'Google',
+                    username: 'john.doe123@googlemail.com',
+                    active: true,
+                    space: {
+                        used: 1024,
+                        available: 15 * 1024 * 1024 * 1024
+                    }
+                }, {
+                    type: 'Google',
+                    username: 'John.Doe@work.com',
+                    active: true,
+                    space: {
+                        used: 960 * 1024 * 1024,
+                        available: 5 * 1024 * 1024 * 1024
+                    }
+                }, {
+                    type: 'Dropbox',
+                    username: 'john.doe123',
+                    active: false,
+                    space: {
+                        used: 14.5 * 1024 * 1024 * 1024,
+                        available: 15 * 1024 * 1024 * 1024
+                    }
+                }]
+            };
+            $scope.folder = {
+                title: 'New Folder',
+                account: 'Gmail', //TODO: Bacakend guy figure out datatype
+                path: [{
+                    name: 'Party',
+                    pid: 1
+                }, {
+                    name: 'Suplies',
+                    pid: 2
+                }],
+                subfolders: [],
+                files: []
+            }
+            for (var i = 0; i < 13; i++) {
+                $scope.folder.subfolders.push('Folder ' + (i + 1));
+            }
+            for (var i = 0; i < 100; i++) {
+                $scope.folder.files.push({
+                    name: 'File  ' + (i + 1),
+                    type: 'word',
+                    account: i % $scope.user.accounts.length
+                });
+            }
+            $scope.toggleSidenav = function() {
+                $mdSidenav('left').toggle();
+            };
+            $scope.user.account_toggle = function(index) {
+                $scope.user.accounts[index].active = !$scope.user.accounts[index].active;
+            };
+			$scope.gotoFolder = function(pid){
+				//TODO: do something
 			}
-			for(var i = 0; i<13; i++) {
-				$scope.folder.subfolders.push('Folder ' + (i+1));
-			}
-			for(var i = 0; i<100; i++) {
-				$scope.folder.files.push({
-					name: 'File  ' + (i+1),
-					type: 'word',
-					account: i % $scope.user.accounts.length
-				});
-			}
-			$scope.toggleSidenav = function() {
-				$mdSidenav('left').toggle();
-			};
-			$scope.user.account_toggle = function(index) {
-				$scope.user.accounts[index].active = !$scope.user.accounts[index].active;
-			}
-		}
-	]);
+        }
+    ]);
 });
