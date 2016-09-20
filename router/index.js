ff0216cd55ac3f8fd345a7e91c6afd25f12cff32
Jeremy Craven <Jeremy@craven.com>
2016-09-20 13:54:55 -0400

    merge

--- router/index.js
+++ router/index.js
@@ -6,6 +6,10 @@ router.use((req, res, next) => {
     next();
 });
 
+router.route('/').get((req, res) => {
+    console.log('Got here');
+});
+
 router.route('/routes').get((req, res) => {
     res.json(router.stack);
 });
