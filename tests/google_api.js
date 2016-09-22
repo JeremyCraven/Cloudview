9799a15c730914bc7cd6d9afa8196e39c40449a0
Adam Loeb <loeb@purdue.edu>
2016-09-22 18:17:47 -0400

    Added a test for putting files, using a stub for the api. Idk if this is good, but it's kinda cool.

--- tests/google_api.js
+++ tests/google_api.js
@@ -14,3 +14,47 @@ describe('Google API', function() {
 	    });
 	});
 });
+
+describe('API Wrapper', function() {
+	
+	google_access.service.files.list = function() {
+
+	}
+	google_access.service.files.get = function() {
+
+	}
+	
+	describe('#put', function() {
+		it('can put file', function(done) {
+
+			google_access.service = new Object();
+			google_access.service.files = new Object();
+			google_access.service.files.create = function(req, cb) {
+				assert('resource' in req); // We have the metadata
+				assert('media' in req); // We have a file
+				assert('auth' in req); // We have an auth token
+				assert('name' in req.resource); // We have a file name
+				assert('body' in req.media); // We have content
+				assert('mimeType' in req.resource && 'mimeType' in req.media
+					&& req.resource.mimeType === req.media.mimeType); // mimeTypes match
+
+				cb(null, {
+					mimeType: req.resource.mimeType,
+					id: 'naaaa',
+					name: req.resource.name,
+					parents: ['naaa_again'],
+					webContentLink: 'notaurl',
+					webViewLink: 'notaurl'
+				});
+			}
+
+			google_access.put_google_file(new Object(), 'test', 
+				{mimeType: 'text/plain', body: 'hello world!'}, function(obj) {
+					assert(obj != null);
+					assert('id' in obj); // Make sure we got an id
+					assert('webContentLink' in obj || 'webViewLink' in obj); // Make sure we have a url
+					done();
+			});
+		});
+	});
+});
