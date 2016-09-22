dbcbc2f9c58fa22a15a26be00f8e19f199754389
Adam Loeb <loeb@purdue.edu>
2016-09-22 19:12:01 -0400

    Add tests for getting files.

--- tests/google_api.js
+++ tests/google_api.js
@@ -14,14 +14,51 @@ describe('Google API', function() {
 	});
 });
 
+
 describe('API Wrapper', function() {
 	
-	google_access.service.files.list = function() {
+	describe('#get', function() {
+		it('provides at least one download/view link', function(done) {
+			google_access.service = new Object();
+			google_access.service.files = new Object();
+			google_access.service.files.list = function(req, cb) {
+				assert('auth' in req); // We have an auth token
+				cb(null, {files: [{id:'na', webViewLink: 'not a url'}]});
+			}
 
-	}
-	google_access.service.files.get = function() {
+			google_access.get_google_files(new Object(), null,//for root
+				null, function(obj) {
+					assert('webViewLink' in obj.files[0] || 'webContentLink' in obj.files[0]);
+					done();
+				});
 
-	}
+		});
+
+		it('paging works', function(done) {
+			google_access.service = new Object();
+			google_access.service.files = new Object();
+			google_access.service.files.list = function(req, cb) {
+				assert('auth' in req); // We have an auth token
+				if ('pageToken' in req) {
+					cb(null, {files: req.pageToken});
+				} else {
+					cb(null, {nextPageToken: 'next', files: 'first'});
+				}
+			}
+
+			google_access.get_google_files(new Object(), null,//for root
+				null, function(obj) {
+					assert('nextPageToken' in obj);
+					google_access.get_google_files(new Object(), null,//for root
+						obj.nextPageToken, function(obj) {
+							assert(!('nextPageToken' in obj));
+							assert('files' in obj);
+							assert(obj.files === 'next');
+							done();
+						});
+				});
+		});
+	});
 	
 	describe('#put', function() {
 		it('can put file', function(done) {
