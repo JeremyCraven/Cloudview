afe6d5154996deb8caad0e3a213b00ff2ef9f28e
Adam Loeb <loeb@purdue.edu>
2016-09-22 22:54:15 -0400

    Add test to make sure authorize_dropbox returns something.

--- tests/api.js
+++ tests/api.js
@@ -1,9 +1,13 @@
 var assert = require('assert');
 var server = require('../server.js')
+var http = require('http');
 
 describe('Our API', function() {
-	it('do nothing', function() {
-
+	it('check authorize dropbox is alive', function(done) {
+		http.get('http://localhost:8081/authorize_dropbox', function(res) {
+			assert.equal(200, res.statusCode);
+      		done();
+		});
 	});
 
 	after(function() {
