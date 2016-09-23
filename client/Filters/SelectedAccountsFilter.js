0140c21a09f78e99e6ce4be72cc7587b9ed79a2f
Richard L. Wisniewski <r.wisniewski1101@gmail.com>
2016-09-23 14:11:12 -0400

    Added tests for filter and optimised it.
    Such bugs I make.

--- client/Filters/SelectedAccountsFilter.js
+++ client/Filters/SelectedAccountsFilter.js
@@ -3,12 +3,34 @@ define([
 ], function(module) {
     return module.filter('selectedAccounts', function() {
         return function(items, accounts) {
-            var filtered = [];
-			angular.forEach(items, function(key, value) {
-				if(accounts[key.account].active)
-					filtered.push(key);
-			});
-            return filtered;
+			function filter() {
+				var filtered = [];
+				angular.forEach(items, function(key, value) {
+					if(accounts[key.account].active)
+						filtered.push(key);
+				});
+				return filtered;
+			}
+			function noactive_test() {
+				for(var i = 0; i < accounts.length; i++) {
+					if(accounts[i].active) {
+						return filter();
+					}
+				}
+				return [];
+			}
+			function allactive_test() {
+				for(var i = 0; i < accounts.length; i++) {
+					if(!accounts[i].active) {
+						if(i > 0)
+							return filter();
+						else
+							return noactive_test();
+					}
+				}
+	            return items;
+			}
+			return allactive_test();
         };
     });
 });
---
+++ client/Test/Filters/SelectedAccountsSpec.js
@@ -0,0 +1,87 @@
+define(['Application', 'angular', 'ngMocks'], function(app, ng, mocks) {
+    describe('testing SelectedAccounts Filter', function() {
+        beforeEach(module('CloudView'));
+
+        var $filter;
+
+        beforeEach(inject(function(_$filter_) {
+            $filter = _$filter_('selectedAccounts');
+        }));
+
+        describe('form validation', function() {
+			var accounts, items;
+			beforeEach(function() {
+				accounts = [{
+                    type: 'Google',
+                    username: 'john.doe123@googlemail.com',
+                    active: true,
+                    space: {
+                        used: 1024,
+                        available: 15 * 1024 * 1024 * 1024
+                    }
+                }, {
+                    type: 'Google',
+                    username: 'John.Doe@work.com',
+                    active: true,
+                    space: {
+                        used: 960 * 1024 * 1024,
+                        available: 5 * 1024 * 1024 * 1024
+                    }
+                }, {
+                    type: 'Dropbox',
+                    username: 'john.doe123',
+                    active: false,
+                    space: {
+                        used: 14.5 * 1024 * 1024 * 1024,
+                        available: 15 * 1024 * 1024 * 1024
+                    }
+                }];
+				items = [];
+				for (var i = 0; i < 100; i++) {
+	                items.push({
+	                    name: 'File  ' + (i + 1),
+	                    type: 'word',
+	                    account: i % accounts.length,
+						pid: i
+	                });
+	            }
+			})
+            it('None should be filtered', function() {
+				for(var i = 0; i < accounts.length; i++)
+					accounts[i].active = true;
+				var result = $filter(items, accounts);
+                expect(result).toEqual(items);
+            });
+            it('NONE SHALL PASS!', function() {
+				for(var i = 0; i < accounts.length; i++)
+					accounts[i].active = false;
+				var result = $filter(items, accounts);
+                expect(result).toEqual([]);
+            });
+            /*it('Only some', function() {
+
+            });*/
+			it('Only middle', function() {
+				for(var i = 0; i < accounts.length; i++)
+					if(i == 0 || i == (accounts.length - 1))
+						accounts[i].active = false;
+					else
+						accounts[i].active = true;
+				var result = $filter(items, accounts);
+				debugger;
+                for(var i = 0; i < result.length; i++)
+					expect(result[i].account > 0 || result[i].account < (accounts.length - 1)).toBe(true);
+            });
+			it('Only Corners', function() {
+				for(var i = 0; i < accounts.length; i++)
+					if(i == 0 || i == (accounts.length - 1))
+						accounts[i].active = true;
+					else
+						accounts[i].active = false;
+				var result = $filter(items, accounts);
+                for(var i = 0; i < result.length; i++)
+					expect(result[i].account == 0 || result[i].account == (accounts.length - 1)).toBe(true);
+            });
+        });
+    });
+});
