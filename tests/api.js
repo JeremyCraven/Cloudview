55cbbde7c2a1a0b4ee0b9194046951e72f807cdb
Adam Loeb <loeb@purdue.edu>
2016-09-25 20:46:52 -0400

    Fix tests I broke.

--- tests/api.js
+++ tests/api.js
@@ -3,8 +3,8 @@ var server = require('../server.js')
 var http = require('http');
 
 describe('Our API', function() {
-	it('check authorize dropbox is alive', function(done) {
-		http.get('http://localhost:8081/authorize_dropbox', function(res) {
+	it('check authorize api is alive', function(done) {
+		http.get('http://localhost:8081/api/v1/login', function(res) {
 			assert.equal(200, res.statusCode);
       		done();
 		});
