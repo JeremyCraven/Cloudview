f5d6d1196c7c3710e62c0f743cadd5f9f5c9f2aa
rwisniewski1101 <r.wisniewski1101@gmail.com>
2016-09-28 18:45:47 -0400

    Cleaned up indent

--- client/Controllers/FolderController.js
+++ client/Controllers/FolderController.js
@@ -1,14 +1,14 @@
 define([
-	'./Module'
+    './Module'
 ], function(module) {
-	return module.controller('CloudView.Controllers.Folder', [
-		'$scope',
-		'$state',
-		'$mdSidenav',
-		'CloudView.Services.FileServices',
-		function FolderController($scope, $state, $mdSidenav, FileServices) {
-			console.log($state.params);
-			$scope.ui = {
+    return module.controller('CloudView.Controllers.Folder', [
+        '$scope',
+        '$state',
+        '$mdSidenav',
+        'CloudView.Services.FileServices',
+        function FolderController($scope, $state, $mdSidenav, FileServices) {
+            console.log($state.params);
+            $scope.ui = {
                 fab: {
                     isOpen: false
                 },
@@ -49,70 +49,68 @@ define([
                         });
                 }
             };
-			$scope.user = {
-				hasName:	false,
-				username:	'',
-				name:		'',
-				accounts: []
-			};
-			$scope.folder = {
-				title: '',
-				account: '', //TODO: Bacakend guy figure out datatype
-				path:	[],
-				subfolders: [],
-				files:	[]
-			};
-			$scope.getFiles = function() {
-				folderID = $state.params;
-				FileServices.getFiles(folderID)
-					.then(
-						function(result) {
-							sort(result.data.files);
-						},
-						function(result) {
-							console.log(result.data);
-						}
-					);
+            $scope.user = {
+                hasName: false,
+                username: '',
+                name: '',
+                accounts: []
+            };
+            $scope.folder = {
+                title: '',
+                account: '', //TODO: Bacakend guy figure out datatype
+                path: [],
+                subfolders: [],
+                files: []
+            };
+            $scope.getFiles = function() {
+                folderID = $state.params;
+                FileServices.getFiles(folderID)
+                    .then(
+                        function(result) {
+                            sort(result.data.files);
+                        },
+                        function(result) {
+                            console.log(result.data);
+                        }
+                    );
 
-			};
+            };
 
 
-			$scope.getFiles();
+            $scope.getFiles();
 
-			var sort = function(files) {
-				files.forEach(function(file) {
-					if (file.isDir) {
-						$scope.folder.subfolders.push(mapFolder(file));
-					}
-					else {
-						$scope.folder.files.push(mapFile(file));
-					}
-				})
-			}
+            var sort = function(files) {
+                files.forEach(function(file) {
+                    if (file.isDir) {
+                        $scope.folder.subfolders.push(mapFolder(file));
+                    } else {
+                        $scope.folder.files.push(mapFile(file));
+                    }
+                })
+            }
 
-			var mapFolder = function(file) {
-				newFile = {};
-				// TODO
-				newFile = file;
-				return newFile;
-			}
+            var mapFolder = function(file) {
+                newFile = {};
+                // TODO
+                newFile = file;
+                return newFile;
+            }
 
-			var mapFile = function(folder) {
-				newFolder = {};
-				// TODO
-				newFolder = folder;
-				return newFolder;
-			}
+            var mapFile = function(folder) {
+                newFolder = {};
+                // TODO
+                newFolder = folder;
+                return newFolder;
+            }
 
-			$scope.open = function(object) {
-				if (object.isDir) {
-					$state.go('folder', object.id);
-				}
-				else {
-					// open file
-				}
-			}
+            $scope.open = function(object) {
+                if (object.isDir) {
+                    $state.go('folder', object.id);
+                } else {
+                    // open file
+                }
+            }
 
-		}
-	]);
+        }
+    ]);
 });
