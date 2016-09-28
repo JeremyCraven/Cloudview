5259e4ef2b934c4b9f85894e930c54441b6e5784
rwisniewski1101 <r.wisniewski1101@gmail.com>
2016-09-28 19:10:09 -0400

    Fixed some of Google Sign In.
    Still getting a 404.  Will try to work out in next meeting.

--- client/Controllers/NewAccountDialogController.js
+++ client/Controllers/NewAccountDialogController.js
@@ -1,34 +1,19 @@
 define([
-	'./Module'
+	'./Module',
+	'google-signin'
 ], function(module) {
 	return module.controller('CloudView.Controllers.Dialog.NewAccount', [
 		'$scope',
 		'$mdDialog',
 		function NewAccountDialogController($scope, $mdDialog) {
-			require(['google-signin'], function(){
-				//lazy load the apis
-				gapi.signin2.render('google-signin', {
-					'scope': 'profile email',
-					'width': 240,
-					'height': 50,
-					'longtitle': true,
-					'theme': 'light',
-					'onsuccess': $scope.google.onSuccess,
-					'onfailure': $scope.google.onFailure
-				});
-			});
 			$scope.google = {
-				onSuccess: function(googleUser) {
+				signin: function(googleUser) {
 					//TODO: tell server
 					if(true) { //TODO: change to if success on our server
 						$scope.hide('success');
 					} else {
 						$scope.hide('reason'); //TODO: change reason for toast
 					}
-				},
-				onFailure: function(error) {
-					//TODO: handle failure
-					$mdDialog.hide('reason'); //TODO: change reason for toast
 				}
 			};
 			$scope.close = function() {
