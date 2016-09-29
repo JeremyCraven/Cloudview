f423f72f77b32f7d6c1257b7a347f172b6c01237
rwisniewski1101 <r.wisniewski1101@gmail.com>
2016-09-28 21:11:30 -0400

    Better house keepting
    That last commit broke things...  Shame on me.

--- client/Controllers/NewAccountDialogController.js
+++ client/Controllers/NewAccountDialogController.js
@@ -1,25 +1,27 @@
 define([
-	'./Module'
+    './Module'
 ], function(module) {
-	return module.controller('CloudView.Controllers.Dialog.NewAccount', [
-		'$scope',
-		'$mdDialog',
-		function NewAccountDialogController($scope, $mdDialog) {
-			//TODO: handle google auto signin
-			$scope.google = {
-				onsuccess: function(googleUser) {
-					//TODO: tell server
-					debugger;
-					if(true) { //TODO: change to if success on our server
-						$mdDialog.hide('success'); //TODO: figure out a response to folder controller to tell it to update the view
-					} else {
-						$mdDialog.hide('reason'); //TODO: change reason for toast
-					}
-				}
-			};
-			$scope.close = function() {
-				$mdDialog.cancel();
-			}
-		}
-	]);
+    return module.controller('CloudView.Controllers.Dialog.NewAccount', [
+        '$scope',
+        '$mdDialog',
+        function NewAccountDialogController($scope, $mdDialog) {
+            //TODO: handle google auto signin
+            $scope.google = {
+                'width': 240,
+                'height': 50,
+                onsuccess: function(googleUser) {
+                    //TODO: tell server
+                    debugger;
+                    if (true) { //TODO: change to if success on our server
+                        $mdDialog.hide('success'); //TODO: figure out a response to folder controller to tell it to update the view
+                    } else {
+                        $mdDialog.hide('reason'); //TODO: change reason for toast
+                    }
+                }
+            };
+            $scope.close = function() {
+                $mdDialog.cancel();
+            }
+        }
+    ]);
 });
