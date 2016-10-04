0bf825917c3da3f71f2eefb9e948a3b489aa3a4c
rwisniewski1101 <r.wisniewski1101@gmail.com>
2016-10-04 14:16:54 -0400

    Fixed Service Dependencies

--- client/Services/FileServices.js
+++ client/Services/FileServices.js
@@ -7,29 +7,30 @@ define([
 			var service = {};
 
 			var url = 'http://localhost:8081/api/v1/';
-			
+
 			service.getFiles = function(credentials) {
+				credentials.token = AccountServices.userAccount.cloudViewToken;
 				return $http({
 					method: 'POST',
 					url: url + 'get_files',
 					data: credentials
 				});
-			}
+			};
 
 			service.authorizeGoogle = function() {
 				return $http({
 					method: 'GET',
 					url: url + 'authorize_google',
 				});
-			}
+			};
 
 			service.authorizeDropbox = function() {
 				return $http({
 					method: 'GET',
 					url: url + 'authorize_dropbox'
 				})
-			}
-			
+			};
+
 			return service;
 		}
 	]);
