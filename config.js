57e4fcb806cc04820e8f94b30e64a8c33e0c2ce4
Adam Loeb <loeb@purdue.edu>
2016-10-29 16:46:12 -0400

    Moved all secrets to one config file

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
 var conf = {
-	CLIENT_ID: "565687073066-b4g8ahiduf3uapmemgkoqdmkctgvevpf.apps.googleusercontent.com",
-	CLIENT_SECRET: "JQsT5qAkWCBFsPUKrHLOVJI-",
-	GOOGLE_CALLBACK: "http://localhost:8081/api/v1/users/auth_google_callback",
+	GOOGLE_ID: "565687073066-b4g8ahiduf3uapmemgkoqdmkctgvevpf.apps.googleusercontent.com",
+	GOOGLE_SECRET: "JQsT5qAkWCBFsPUKrHLOVJI-",
+	ONEDRIVE_CLIENT_ID: "58054cc9-75fc-4e79-bfa2-8dac7656afbf",
+	ONEDRIVE_CLIENT_SECRET: "LYFRbjrb5J5bjiRqmfaNY3b",
 	DROPBOX_ID: "mbvg6drwcnt8ijt",
 	DROPBOX_SECRET: "o5dd4i6tp2ku5l9",
-	DROPBOX_CALLBACK: "http://localhost:8081/api/v1/users/auth_dropbox_callback",
 	TOKEN_SECRET: 'AdJeMiTaRa',
-	GOOGLE_AUTH_REDIRECT_URL: "urn:ietf:wg:oauth:2.0:oob"
+	GOOGLE_CALLBACK: "http://localhost:8081/api/v1/users/auth_google_callback",
+	DROPBOX_CALLBACK: "http://localhost:8081/api/v1/users/auth_dropbox_callback",
+	GOOGLE_AUTH_REDIRECT_URL: "urn:ietf:wg:oauth:2.0:oob",
+	ONEDRIVE_CALLBACK: "http://localhost:8081/api/v1/users/auth_onedrive_callback"
 };
 
 module.exports = conf;
